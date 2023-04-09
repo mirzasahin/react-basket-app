e08f2a203c4e07dacb870c6fac039ff706a65148
Mirza Sahin <salihmirza123@gmail.com>
2023-04-10 01:19:31 +0300

    Add photos and other card details

--- src/App.js
+++ src/App.js
@@ -6,16 +6,34 @@ import "./App.css";
 
 const storeItems = [
   {
-    name: "Çikolata",
-    price: "20",
+    name: "Camera Lens",
+    src: "camera-lens.jpg",
+    price: "$420",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
   },
   {
-    name: "Masa Lambası",
-    price: "10",
+    name: "DSLR Camera",
+    src: "camera.jpg",
+    price: "$950",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
   },
   {
-    name: "Duvar Saati",
-    price: "15",
+    name: "Headphones",
+    src: "headphones.jpg",
+    price: "$45",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+  },
+  {
+    name: "Shoes",
+    src: "shoes.jpg",
+    price: "$95",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+  },
+  {
+    name: "Watch",
+    src: "watch.jpg",
+    price: "$200",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
   },
 ];
 
@@ -28,24 +46,31 @@ function App() {
 
   return (
     <Container>
+
+      <Input.Wrapper>
+        <Input
+          pt={20}
+          p={20}
+          placeholder="Ara"
+          onChange={(e) => setSearchValue(e.target.value)}
+        ></Input>
+      </Input.Wrapper>
+
       <SimpleGrid cols={3} className="store">
-        {storeItems.map(({ name, price }) => {
+        {storeItems.map(({ name, price, src, description }) => {
           return (
             <Card
               key={name}
               name={name}
               price={price}
+              src={src}
+              description={description}
               onAdd={() => setBasketItems([...basketItems, { name }])}
             />
           );
         })}
       </SimpleGrid>
-      <Input.Wrapper>
-        <Input
-          placeholder="Ara"
-          onChange={(e) => setSearchValue(e.target.value)}
-        ></Input>
-      </Input.Wrapper>
+
       <List
         className="list"
         spacing="xs"
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,11 @@
 import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
 
-function CardComponent({ name, price, onAdd }) {
+function CardComponent({ name, price, onAdd, src, description }) {
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
       <Card.Section>
         <Image
-          src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+          src={`/assets/images/${src}`}
           height={160}
           alt="Norway"
         />
@@ -13,13 +13,13 @@ function CardComponent({ name, price, onAdd }) {
 
       <Group position="apart" mt="md" mb="xs">
         <Text weight={500}>{name}</Text>
-        <Badge color="pink" variant="light">
-          On Sale
+        <Badge color="red" variant="light">
+          {price}
         </Badge>
       </Group>
 
       <Text size="sm" color="dimmed">
-        {price}
+        {description}
       </Text>
 
       <Button
