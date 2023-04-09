268bb521cebb9e0f07cc1336ecfc3f4a0a5bd6c1
Mirza Sahin <salihmirza123@gmail.com>
2023-04-09 23:18:58 +0300

    Add project files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Container, SimpleGrid, List, ThemeIcon, Input } from "@mantine/core";
+import { IconCircleCheck } from "@tabler/icons-react";
+import Card from "./Components/Card";
+import "./App.css";
+
+const storeItems = [
+  {
+    name: "Çikolata",
+    price: "20",
+  },
+  {
+    name: "Masa Lambası",
+    price: "10",
+  },
+  {
+    name: "Duvar Saati",
+    price: "15",
+  },
+];
 
 function App() {
+  let [basketItems, setBasketItems] = useState([]);
+  let [searchValue, setSearchValue] = useState("");
+  let filteredItems = basketItems.filter(
+    (item) => item.name.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <SimpleGrid cols={3} className="store">
+        {storeItems.map(({ name, price }) => {
+          return (
+            <Card
+              key={name}
+              name={name}
+              price={price}
+              onAdd={() => setBasketItems([...basketItems, { name }])}
+            />
+          );
+        })}
+      </SimpleGrid>
+      <Input.Wrapper>
+        <Input
+          placeholder="Ara"
+          onChange={(e) => setSearchValue(e.target.value)}
+        ></Input>
+      </Input.Wrapper>
+      <List
+        className="list"
+        spacing="xs"
+        size="sm"
+        center
+        icon={
+          <ThemeIcon color="teal" size={24} radius="xl">
+            <IconCircleCheck size="1rem" />
+          </ThemeIcon>
+        }
+      >
+        {filteredItems.map(
+          (
+            { name },
+            index // aynı itemden birden fazla oluşturulabileceği için key hatası alırız. bu yüzden key=index no yaparız.
+          ) => (
+            <List.Item key={index}>{name}</List.Item> // map function'ın ikinci aldığı parametre index no'dur.
+          )
+        )}
+      </List>
+    </Container>
   );
 }
 
---
+++ src/Components/Card.js
@@ -0,0 +1,39 @@
+import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
+
+function CardComponent({ name, price, onAdd }) {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Card.Section>
+        <Image
+          src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+          height={160}
+          alt="Norway"
+        />
+      </Card.Section>
+
+      <Group position="apart" mt="md" mb="xs">
+        <Text weight={500}>{name}</Text>
+        <Badge color="pink" variant="light">
+          On Sale
+        </Badge>
+      </Group>
+
+      <Text size="sm" color="dimmed">
+        {price}
+      </Text>
+
+      <Button
+        variant="light"
+        color="blue"
+        fullWidth
+        mt="md"
+        radius="md"
+        onClick={onAdd}
+      >
+        Ekle
+      </Button>
+    </Card>
+  );
+}
+
+export default CardComponent;
