1beb7bbca8a8bffd8b12ed085da371f0affe8860
Mirza Sahin <salihmirza123@gmail.com>
2023-04-10 03:19:56 +0300

    Add clear button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
-import { Container, SimpleGrid, List, ThemeIcon, Input } from "@mantine/core";
+import {
+  Container,
+  SimpleGrid,
+  List,
+  ThemeIcon,
+  Input,
+  Flex,
+  Button,
+  Grid,
+  Group,
+} from "@mantine/core";
 import { IconCircleCheck } from "@tabler/icons-react";
 import Card from "./Components/Card";
 import "./App.css";
@@ -9,55 +19,59 @@ const storeItems = [
     name: "Camera Lens",
     src: "camera-lens.jpg",
     price: "$420",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
   },
   {
     name: "DSLR Camera",
     src: "camera.jpg",
     price: "$950",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
   },
   {
     name: "Headphones",
     src: "headphones.jpg",
     price: "$45",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
   },
   {
     name: "Shoes",
     src: "shoes.jpg",
     price: "$95",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
   },
   {
     name: "Watch",
     src: "watch.jpg",
     price: "$200",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
   },
 ];
 
 function App() {
   let [basketItems, setBasketItems] = useState([]);
   let [searchValue, setSearchValue] = useState("");
-  let filteredItems = basketItems.filter(
+  let filteredItems = storeItems.filter(
     (item) => item.name.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0
   );
 
   return (
     <Container>
 
-      <Input.Wrapper>
-        <Input
-          pt={20}
-          p={20}
-          placeholder="Ara"
-          onChange={(e) => setSearchValue(e.target.value)}
-        ></Input>
-      </Input.Wrapper>
+        <Grid className="clear">
+
+          <Grid.Col span={10}><Input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} placeholder="Ara"/></Grid.Col>
+          <Grid.Col span={2}><Button onClick={() => setSearchValue('')} variant="filled" color="blue" style={{ width: '100%' }}>Sıfırla</Button></Grid.Col>
+    
+        </Grid>
+  
 
       <SimpleGrid cols={3} className="store">
-        {storeItems.map(({ name, price, src, description }) => {
+        {filteredItems.map(({ name, price, src, description }) => {
           return (
             <Card
               key={name}
@@ -82,7 +96,7 @@ function App() {
           </ThemeIcon>
         }
       >
-        {filteredItems.map(
+        {basketItems.map(
           (
             { name },
             index // aynı itemden birden fazla oluşturulabileceği için key hatası alırız. bu yüzden key=index no yaparız.
